ccfb334ff5dcd8d7c5b9e854c4a5b9ac70bf1407
gyamfi samuel <gyamfisam71@gmail.com>
2022-09-07 11:29:54 +0000

    Test user data changed

--- src/data/testdata.js
+++ src/data/testdata.js
@@ -1,8 +1,7 @@
 const fakeUser = {
     id : 1,
     displayName : "Gyamfi Samuel",
-    photoURL : "img/IMG_1553.jpg",
+    photoURL : "",
     email : "keepingnice@gmail.com",
-    uid : "SQWKgJGORZcd8TPsqzpC3V1Ftf42",
     uuid : "9499cb33-f6d1-45bb-bb9c-bbf0814787ce",
 }
